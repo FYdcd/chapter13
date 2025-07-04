e79026258324423877d272d006614a3c3ce92399
241005 <241005@std.hi-joho.ac.jp>
2025-07-04 11:13:25 +0900

    chapter13

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.hp = 40;
+        h.name = "ミナト";
+        System.out.println(h.name + "の現在のHP: " + h.hp);
+
+        Inn i = new Inn();
+        System.out.println("宿屋で回復中...");
+        i.checkIn(h);
+        System.out.println(h.name + "の現在のHP: " + h.hp);
+
+        King k = new King();
+        k.talk(h);//chapter13
+    }
+}
